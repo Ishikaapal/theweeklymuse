cb5b5f2e61ede010cf8078e58fd57634e4df528b
ishikapal <ishh.tech08@gmail.com>
2025-06-05 23:45:44 +0530

    Switch to PostgreSQL

--- theweeklymuse/settings.py
+++ theweeklymuse/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,14 +86,12 @@ WSGI_APPLICATION = 'theweeklymuse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
